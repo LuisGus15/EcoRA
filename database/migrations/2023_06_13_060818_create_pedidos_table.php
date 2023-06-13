3bc564bf10b7537bacc601b4b87982ba378a18d9
LuisGus15 <gustavito1520@gmail.com>
2023-06-13 04:46:15 -0400

    ga

---
+++ database/migrations/2023_06_13_060818_create_pedidos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->date('fechaPedido');
+            $table->date('fechaEntreaga');
+            $table->string('entraga');
+            $table->string('direccion');
+            $table->string('recargo');
+            $table->string('monto');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
